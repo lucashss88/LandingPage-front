467f96a1b57f2116bd0d3331404479f6736e346b
lucashss88 <lucashss88@gmail.com>
2025-05-02 10:25:16 -0300

    adding simulate videos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // src/App.js
 import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 import { motion } from 'framer-motion';
 import './App.css';
 import './index.css';
-import img from './assets/images/lhss_33333.jpg';
-import tech from './assets/images/tech@2x.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import tech_inv from './assets/images/tech_inv@2x.png';
 import profile from './assets/images/profile2.jpg';
 import { FaHome } from 'react-icons/fa';
@@ -16,138 +15,126 @@ import ProjectCarousel from './ProjectCarousel';
 
 function App() {
   return (
-    <div className="App">
-    {/* Navbar */}
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="header" smooth={true} duration={500}>
-              <FaHome className="icons"/>
-                HOME 
-            </Link>
-          </li>
-          <li>
-            <Link to="sobre-mim" smooth={true} duration={500}>
-                <FaUser className="icons"/>
-                SOBRE MIM
-            </Link>
-          </li>
-          <li>
-            <Link to="projetos" smooth={true} duration={500}>
-                <FaDesktop className="icons"/>
-                PROJETOS
-            </Link>
-          </li>
-          <li>
-            <Link to="contate-me" smooth={true} duration={500}>
-                <FaAddressBook className="icons"/>
-                CONTATE-ME
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <header className="header" id="header">
-        <div>
-          <h1>LUCAS HENRIQUE SERRANO SOARES</h1>
-          {/*<img src={profile} className="img_profile" />*/}
-        </div>
-        <img src={tech_inv} className="img_banner" />
-      </header>
-        
-      {/*<div>*/}
-      {/*  <img src={tech} className="img_banner"/>*/}
-      {/*</div>*/}
-        
-      {/* Seção Sobre Mim com animação */}
-      <motion.section
-        className="about"
-        id="sobre-mim"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <h2>Sobre Mim</h2>
-        <img src={profile} className="img_profile" />
-        <p>
-            Sou estudante do último período de Tecnologia em Sistemas para Internet (IFPB), com experiência prática em desenvolvimento front-end e projetos fullstack.
-            Tenho interesse em atuar com tecnologias como React, Node.js e PostgreSQL. Durante meu estágio, desenvolvi interfaces e sistemas com foco em usabilidade,
-            colaboração em equipe e boas práticas ágeis. Busco oportunidades para continuar aprendendo e contribuindo com soluções inovadoras, tanto no desenvolvimento web quanto mobile.
-        </p>
-        <h2>Experiências</h2>
-        <p>
-              Durante meu estágio na Assert IFPB, desenvolvi interfaces de usuário para aplicações web utilizando TypeScript,
-              HTML, CSS e React. Participei do desenvolvimento de um armário inteligente, contribuindo com soluções criativas
-              para otimizar a experiência do usuário. Trabalhei em uma equipe ágil,
-              utilizando Git para controle de versão e Scrum para organização e monitoramento das atividades no Trello,
-              aprimorando minhas habilidades de colaboração e trabalho em equipe.  
-        </p>
-        <h2>Habilidades e tecnologias</h2>
-        <div className="list_tec">
+     <div className="App">
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top px-3 py-lg-3 w-100">
+         <div className="container-fluid d-flex justify-content-center">
+           <ul className="navbar-nav d-flex gap-5 cursor-pointer">
+             <li className="nav-item nav-link text-white">
+               <Link to="header" smooth={true} duration={500}>
+                 <FaHome className="icons"/>
+                   HOME
+               </Link>
+             </li>
+             <li className="nav-item nav-link text-white">
+               <Link to="sobre-mim" smooth={true} duration={500}>
+                 <FaUser className="icons"/>
+                 SOBRE MIM
+               </Link>
+             </li>
+             <li className="nav-item nav-link text-white">
+               <Link to="projetos" smooth={true} duration={500}>
+                 <FaDesktop className="icons"/>
+                 PROJETOS
+               </Link>
+             </li>
+             <li className="nav-item nav-link text-white">
+               <Link to="contate-me" smooth={true} duration={500}>
+                 <FaAddressBook className="icons"/>
+                 CONTATE-ME
+               </Link>
+             </li>
+           </ul>
+         </div>
+       </nav>
+       <header id="header" className="bg-dark text-white text-center p-5 mt-5 mb-2">
+           {/* eslint-disable-next-line jsx-a11y/alt-text */}
+         <img src={tech_inv} className="img_banner" />
+       </header>
+       <motion.section
+          className="about"
+          id="sobre-mim"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+       >
+          <h2>Sobre Mim</h2>
+          <img src={profile} className="img_profile rounded-5 mb-3"  alt={profile}/>
+          <p>
+              Sou estudante do último período de Tecnologia em Sistemas para Internet (IFPB), com experiência prática em desenvolvimento front-end e projetos fullstack.
+              Tenho interesse em atuar com tecnologias como React, Node.js e PostgreSQL. Durante meu estágio, desenvolvi interfaces e sistemas com foco em usabilidade,
+              colaboração em equipe e boas práticas ágeis. Busco oportunidades para continuar aprendendo e contribuindo com soluções inovadoras, tanto no desenvolvimento web quanto mobile.
+          </p>
+          <h2>Experiências</h2>
+          <p>
+            Durante meu estágio na Assert IFPB, desenvolvi interfaces de usuário para aplicações web utilizando TypeScript,
+            HTML, CSS e React. Participei do desenvolvimento de um armário inteligente, contribuindo com soluções criativas
+            para otimizar a experiência do usuário. Trabalhei em uma equipe ágil,
+            utilizando Git para controle de versão e Scrum para organização e monitoramento das atividades no Trello,
+            aprimorando minhas habilidades de colaboração e trabalho em equipe.
+          </p>
+          <h2>Habilidades e tecnologias</h2>
+          <div className="list_tec">
             <ul>
-                <li>
-                    Linguagens de Programação<br />
-                    - JavaScript e TypeScript: Usados em projetos com React e Node.js.<br />
-                    - Kotlin: Utilizado em projeto mobile (disciplinar) com Android Studio.<br />
-                    - Java: Conhecimento básico obtido em disciplinas da graduação.<br />
-                </li>
-                <li>
-                    Frameworks e Bibliotecas<br />
-                    - ReactJS: Aplicado no estágio (armário inteligente) e em projetos pessoais.<br />
-                    - VueJS: Utilizado em parte do sistema desenvolvido no estágio.<br />
-                    - Angular: Conhecimento obtido em projetos acadêmicos e cursos.<br />
-                </li>
-                <li>
-                    Desenvolvimento Mobile<br />
-                    - Android Studio e Kotlin: Projeto final de disciplina de Programação Mobile.<br />
-                    - React Native: Em fase de estudo, com base na experiência com React.<br />
-                </li>
-                <li>
-                    Web Design<br />
-                    - HTML, CSS, Tailwind, Bootstrap: Utilizados em interfaces de diversos projetos.
-                </li>
-                <li>
-                    Bancos de Dados<br />
-                    - MySQL e PostgreSQL: Usados em projetos acadêmicos e pessoais (NutriTrack/FitTrack).
-                </li>
-                <li>
-                    Controle de Versão<br />
-                    - Git, GitHub, GitLab: Versionamento e colaboração em todos os projetos.
-                </li>
-                <li>
-                    Metodologias Ágeis<br />
-                    - Scrum e Kanban: Aplicados no estágio (Trello) e em projetos da graduação.
-                </li>
+              <li>
+                Linguagens de Programação<br />
+                  - JavaScript e TypeScript: Usados em projetos com React e Node.js.<br />
+                  - Kotlin: Utilizado em projeto mobile (disciplinar) com Android Studio.<br />
+                  - Java: Conhecimento básico obtido em disciplinas da graduação.<br />
+              </li>
+              <li>
+                Frameworks e Bibliotecas<br />
+                  - ReactJS: Aplicado no estágio (armário inteligente) e em projetos pessoais.<br />
+                  - VueJS: Utilizado em parte do sistema desenvolvido no estágio.<br />
+                  - Angular: Conhecimento obtido em projetos acadêmicos e cursos.<br />
+              </li>
+              <li>
+                Desenvolvimento Mobile<br />
+                  - Android Studio e Kotlin: Projeto final de disciplina de Programação Mobile.<br />
+                  - React Native: Em fase de estudo, com base na experiência com React.<br />
+              </li>
+              <li>
+                Web Design<br />
+                  - HTML, CSS, Tailwind, Bootstrap: Utilizados em interfaces de diversos projetos.
+              </li>
+              <li>
+                Bancos de Dados<br />
+                  - MySQL e PostgreSQL: Usados em projetos acadêmicos e pessoais (NutriTrack/FitTrack).
+              </li>
+              <li>
+                Controle de Versão<br />
+                  - Git, GitHub, GitLab: Versionamento e colaboração em todos os projetos.
+              </li>
+              <li>
+                Metodologias Ágeis<br />
+                  - Scrum e Kanban: Aplicados no estágio (Trello) e em projetos da graduação.
+              </li>
             </ul>
-        </div>
-        
-      </motion.section>
+          </div>
+       </motion.section>
 
-      {/* Seção Projetos com animação para cada projeto */}
-      <section className="projects" id="projetos">
-        <h2>Projetos</h2>
 
-        <ProjectCarousel />
-      </section>
+       <section className="projects" id="projetos">
+         <h2>Projetos</h2>
+         <ProjectCarousel />
+       </section>
         
-      {/* Seção Contate-me com animação */}
-      <motion.section
-        className="contact"
-        id="contate-me"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <h2>Contate-me</h2>
-        <p>Email: serrano.lucas@academico.ifpb.edu.br</p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/lucas-henrique-serrano-soares-382339248/">Meu LinkedIn</a></p>
-        <p>Github: <a href="https://github.com/lucashss88">Meu Github</a></p>
-      </motion.section>
+       <motion.section
+          className="contact"
+          id="contate-me"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+       >
+         <h2>Contate-me</h2>
+         <p>Email: serrano.lucas@academico.ifpb.edu.br</p>
+         <p>LinkedIn: <a href="https://www.linkedin.com/in/lucas-henrique-serrano-soares-382339248/">Meu LinkedIn</a></p>
+         <p>Github: <a href="https://github.com/lucashss88">Meu Github</a></p>
+       </motion.section>
         
-      <footer className="footer">
-        <p>&copy; 2024 - Lucas Henrique</p>
-      </footer>
-    </div>
+       <footer className="text-center p-3 bg-dark text-white mb-0 position-relative">
+         <p>&copy; 2025 - LUCAS HENRIQUE SERRANO SOARES</p>
+       </footer>
+     </div>
   );
 }
 
--- src/ProjectCarousel.js
+++ src/ProjectCarousel.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './ProjectCarousel.css';
-import img from './assets/images/tech@2x.png';
 import nt from './assets/images/Captura de tela 2024-09-13 142836.png';
 import ntPopup from './assets/images/Captura de tela 2024-08-19 114610.png';
 import ntPopup2 from './assets/images/Captura de tela 2024-10-09 102057.png';
 import gf from './assets/images/Captura de tela 2025-04-01 152504.png';
 import gymfit from './assets/images/Captura de tela 2024-10-14 202747.png';
 import gymfit2 from './assets/images/Captura de tela 2024-10-14 202804.png';
-import { FaAngleLeft } from 'react-icons/fa';
-import { FaAngleRight } from 'react-icons/fa';
+import sf from './assets/images/Captura de tela 2025-04-29 114635.png';
+import studyflow from './assets/images/Captura de tela 2025-04-29 114729.png';
+import studyflow2 from './assets/images/Captura de tela 2025-04-29 114754.png';
 
 const projects = [
     {
@@ -18,30 +18,52 @@ const projects = [
         description: 'O NutriTrack é um sistema desenvolvido para auxiliar nutricionistas e pacientes no gerenciamento de' +
             ' dietas e informações nutricionais. Permite o cadastro de alimentos, criação de dietas personalizadas, substituição de refeições e visualização detalhada por parte dos pacientes.\n' +
             '\n' +
-            'Tecnologias utilizadas:\n' +
             'Tecnologias: React (Router, Hooks), Tailwind, Node.js, Express, JWT, PostgreSQL (Sequelize).\n' +
             '\n' +
             'Repositórios:\n' +
             '- Frontend:\n' +
-            '- Backend:',
+            '- Backend:\n' +
+            '- Vídeo com simulacão: ',
         image: nt,
         imagePopup: ntPopup,
         imagePopup2: ntPopup2,
+        simulacao: 'https://www.loom.com/share/fc900a72af774674bcccf63eb80867fb?sid=4fcf993f-7505-42cd-8d86-559f1d3cc75e',
         // deployLink: 'https://nutri-track-front.vercel.app'
     },
     {
         id: 2,
         title: 'FitTrack',
         description: 'O FitTrack é uma plataforma web/mobile para organização de treinos e atividades físicas, com criação de treinos personalizados e edição de perfil.\n' +
+            '\n' +
             'Tecnologias: React (Router, Hooks), Tailwind CSS, Node.js, Express, JWT, PostgreSQL (Sequelize).\n'+
             '\n' +
             'Repositórios:\n' +
             '- Frontend:\n' +
-            '- Backend:',
+            '- Backend:\n' +
+            '- Vídeo com simulacão: ',
         image: gf,
         imagePopup: gymfit,
         imagePopup2: gymfit2,
+        simulacao: 'https://www.loom.com/share/a875df19bae04ad8a7ea37159a76c68a',
         // deployLink: 'https://gym-prive-front.vercel.app'
+    },
+    {
+        id: 3,
+        title: 'StudyFlow',
+        description: 'O StudyFlow é uma aplicação web para organização de estudos, com foco em produtividade e acompanhamento de atividades.\n' +
+            'Permite cadastro de disciplinas, controle de tempo por atividade (cronômetro), visualização gráfica do desempenho, e autenticação de usuários.\n' +
+            '\n' +
+            'Tecnologias: React (Router, Hooks), Bootstrap 5.3, Node.js, Express, JWT, Sequelize (SQLite).\n' +
+            '\n' +
+            'Repositórios:\n' +
+            '- Frontend:\n' +
+            '- Backend:\n' +
+            '- Vídeo com simulacão: ',
+        image: sf,
+        imagePopup: studyflow,
+        imagePopup2: studyflow2,
+        simulacao: ' https://www.loom.com/share/9429088703f246308c1618b4763fc694?sid=176aeba1-0db9-44fb-bde3-c61835bf08d2',
+        // deployLink: 'https://studyflow.vercel.app'
     },
 ];
 
@@ -76,25 +98,24 @@ const ProjectCarousel = () => {
                 className="carousel"
                 initial={{ opacity: 0, x: 300 }}
                 animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.5 }}
-                key={currentProject} // Isso garante que o card atualize quando o índice mudar
+                key={currentProject}
             >
                 <motion.div
                     className="project-card active"
                     onClick={() => openPopup(currentProject)}
-                    whileHover={{ scale: 1.05 }}
+                    whileHover={{ scale: 1.02 }}
                 >
                     <img src={projects[currentProject].image} alt={projects[currentProject].title} />
                     <h3>{projects[currentProject].title}</h3>
                 </motion.div>
             </motion.div>
 
-            <div className="carousel-controls">
-                <a onClick={handlePrevProject}><FaAngleLeft /></a>
-                <a onClick={handleNextProject}><FaAngleRight /></a>
+            <div className="carousel-controls mt-2 d-flex justify-content-between">
+                <a onClick={handlePrevProject} className="btn btn-dark carousel-control-prev-icon p-3 rounded-2 me-3" />
+                <a onClick={handleNextProject} className="btn btn-dark carousel-control-next-icon p-3 rounded-2 ms-3" />
             </div>
 
-            <div className="carousel-indicators">
+            <div className="carousel-indicators d-flex justify-content-center mt-3">
                 {projects.map((_, index) => (
                     <span
                         key={index}
@@ -122,11 +143,10 @@ const ProjectCarousel = () => {
                                 textAlign: 'center',
                                 whiteSpace: 'pre-line'
                             }}>
-                                {projects[currentProject].description}
+                                {projects[currentProject].description}<a href={projects[currentProject].simulacao}>Link</a>
                             </div>
-                            {/*<a href={projects[currentProject].deployLink} target="_blank" rel="noopener noreferrer">Ver Deploy</a>*/}
                         </div>
-                        <button onClick={closePopup}>Fechar</button>
+                        <button className="btn btn-danger" onClick={closePopup}>Fechar</button>
                     </div>
                 </div>
             )}
